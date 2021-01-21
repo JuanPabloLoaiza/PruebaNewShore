24bf2c551afaf2c14a0516c27e465287303b3b14
Juan Pablo Loaiza <j.loaiza@superpagos.com>
2021-01-21 16:04:47 -0500

    Consumiendo las APIs de las casas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+      <b-row>
+          <b-col cols="3">
+
+          </b-col>
+      </b-row>
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            gryffindorHouse: null,
+            slytherinHouse: null,
+            ravenclawHouse: null,
+            hufflepuffHouse: null
+        }
+    },
+    created() {
+        this.Houses();
+    },
+    methods: {
+        async Houses() {
+            let data = await axios.get('http://hp-api.herokuapp.com/api/characters/house/gryffindor');
+            this.gryffindorHouse = data.data;
+            data = await axios.get('http://hp-api.herokuapp.com/api/characters/house/slytherin');
+            this.slytherinHouse = data.data;
+            data = await axios.get('http://hp-api.herokuapp.com/api/characters/house/ravenclaw');
+            this.ravenclawHouse = data.data;
+            data = await axios.get('http://hp-api.herokuapp.com/api/characters/house/hufflepuff');
+            this.hufflepuffHouse = data.data;
+        }
+    }
 }
 </script>
+
+<style>
+
+</style>
