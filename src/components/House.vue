77a0e74a23d18be2f999e4e39c2a7dc6418c5d9f
Juan Pablo Loaiza <j.loaiza@superpagos.com>
2021-01-21 20:21:07 -0500

    Imagenes de las casas

---
+++ src/components/House.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-container>
+      {{ house }}
+  </b-container>
+</template>
+
+<script>
+import axios from "axios";
+import store from "vuex";
+export default {
+    props: {
+        house: String
+    },
+    created() {
+        this.HouseInformation();
+    }, 
+    methods: {
+        async HouseInformation() {
+            let data = await axios.get(`http://hp-api.herokuapp.com/api/characters/house/${this.house}`);
+            this.$store.state.houseStudents = data.data;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,41 @@
 <template>
   <b-container>
+      <b-row class="mt-4">
+        <b-col cols="3">
+          <img src="../assets/Images/Gryffindor.png" width="100%" @click="Houses('gryffindor')"/>
+        </b-col>
+        <b-col cols="3">
+          <img src="../assets/Images/Slytherin.png" width="100%" @click="Houses('slytherin')"/>
+        </b-col>
+        <b-col cols="3">
+          <img src="../assets/Images/Ravenclaw.png" width="100%" @click="Houses('ravenclaw')"/>
+        </b-col>
+        <b-col cols="3">
+          <img src="../assets/Images/Hufflepuff.png" width="100%" @click="Houses('hufflepuff')"/>
+        </b-col>
+      </b-row>
       <b-row>
           <b-col cols="3">
-
+              <House :house="houseName" v-if="houseName !== null"></House>
           </b-col>
       </b-row>
   </b-container>
 </template>
 
 <script>
-import axios from "axios";
+import House from '@/components/House';
 export default {
+    components: {
+        House
+    },
     data() {
         return {
-            gryffindorHouse: null,
-            slytherinHouse: null,
-            ravenclawHouse: null,
-            hufflepuffHouse: null
+            houseName: null
         }
     },
-    created() {
-        this.Houses();
-    },
     methods: {
-        async Houses() {
-            let data = await axios.get('http://hp-api.herokuapp.com/api/characters/house/gryffindor');
-            this.gryffindorHouse = data.data;
-            data = await axios.get('http://hp-api.herokuapp.com/api/characters/house/slytherin');
-            this.slytherinHouse = data.data;
-            data = await axios.get('http://hp-api.herokuapp.com/api/characters/house/ravenclaw');
-            this.ravenclawHouse = data.data;
-            data = await axios.get('http://hp-api.herokuapp.com/api/characters/house/hufflepuff');
-            this.hufflepuffHouse = data.data;
+        Houses(house) {
+            this.houseName = house;
         }
     }
 }
