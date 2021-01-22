0440b59821360d9100c19632f323842bc93c0175
Juan Pablo Loaiza <j.loaiza@superpagos.com>
2021-01-22 01:12:55 -0500

    Busqueda

--- src/components/House.vue
+++ src/components/House.vue
@@ -1,6 +1,46 @@
 <template>
   <b-container>
-      {{ house }}
+    <b-row class="mt-5">
+      <b-col cols="3">
+        <b-form-input
+          v-model="findCharacter"
+          placeholder="Buscar"
+        ></b-form-input>
+      </b-col>
+      <b-col cols="2" align-self="end">
+        <p>Ordenar por: </p>
+      </b-col>
+      <b-col cols="1">
+        <b-button variant="outline-primary" @click="sortName()">nombre</b-button>
+      </b-col>
+      <b-col cols="1">
+        <b-button variant="outline-primary">apellido</b-button>
+      </b-col>
+    </b-row>
+    <b-row class="mt-5" v-if="members.length === 0">
+      <b-col
+        cols="3"
+        v-for="data in $store.state.houseStudents"
+        :key="data.name"
+        class="mt-3"
+      >
+        <b-card tag="article" class="mb-3" style="max-width: 20rem;">
+          <b-card-title class="titulos">{{ data.name }}</b-card-title>
+          <img :src="data.image" class="imagenes" />
+          <b-card-text class="mt-3">Hola, soy {{ data.name }} </b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
+
+    <b-row class="mt-5" v-else>
+      <b-col cols="3" v-for="data in members" :key="data.name" class="mt-3">
+        <b-card tag="article" class="mb-3" style="max-width: 20rem;">
+          <b-card-title class="titulos">{{ data.name }}</b-card-title>
+          <img :src="data.image" class="imagenes" />
+          <b-card-text class="mt-3">Hola, soy {{ data.name }} </b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -8,21 +48,77 @@
 import axios from "axios";
 import store from "vuex";
 export default {
-    props: {
-        house: String
-    },
-    created() {
-        this.HouseInformation();
-    }, 
-    methods: {
-        async HouseInformation() {
-            let data = await axios.get(`http://hp-api.herokuapp.com/api/characters/house/${this.house}`);
-            this.$store.state.houseStudents = data.data;
+  data() {
+    return {
+      findCharacter: "",
+      members: [],
+    };
+  },
+  props: {
+    house: String,
+  },
+  watch: {
+    findCharacter(value) {
+      let names = [];
+      let lastNames = [];
+      let count = 0;
+      this.members = [];
+      this.$store.state.houseStudents.map((data) => {
+        let firstName = data.name.split(" ");
+        names.push(firstName[0]);
+        lastNames.push(firstName[1]);
+      });
+      this.$store.state.houseStudents.map((data) => {
+        if (value.toLowerCase() === names[count].substring(0, value.length).toLowerCase()) {
+          this.members.push(data);
         }
+        if (value.toLowerCase() === lastNames[count].substring(0, value.length).toLowerCase()) {
+          this.members.push(data);
+        }
+        count++;
+      });
+      this.members = [...new Set(this.members)];
+    },
+  },
+  methods: {
+    sortName() {
+      let names = [];
+      this.members = [];
+      this.$store.state.houseStudents.map((data) => {
+        let firstName = data.name.split(" ");
+        names.push(firstName[0]);
+      });
+      names.sort();
+      names.map((data => {
+        for (let index = 0; index < this.$store.state.houseStudents.length; index++) {
+          console.log("Entra");
+          let firstName = this.$store.state.houseStudents[index].name.split(" ");
+          if (firstName === data) {
+            console.log("Entra al if");
+            members[index] = this.$store.state.houseStudents[index];
+            break;
+          }       
+        }
+      }))
+      console.log(names);
     }
-}
+  }
+};
 </script>
 
-<style>
+<style lang="sass">
 
+@font-face
+    font-family: 'Harry P'
+    src: url("../assets/fonts/HARRYP.TTF") format('truetype')
+
+$font-title: Harry P, Harry P
+
+.imagenes
+    object-fit: scale-down
+    width: 100%
+    height: 30vh
+
+.titulos
+    font: 200% $font-title
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,61 @@
 <template>
   <b-container>
-      <b-row class="mt-4">
-        <b-col cols="3">
-          <img src="../assets/Images/Gryffindor.png" width="100%" @click="Houses('gryffindor')"/>
-        </b-col>
-        <b-col cols="3">
-          <img src="../assets/Images/Slytherin.png" width="100%" @click="Houses('slytherin')"/>
-        </b-col>
-        <b-col cols="3">
-          <img src="../assets/Images/Ravenclaw.png" width="100%" @click="Houses('ravenclaw')"/>
-        </b-col>
-        <b-col cols="3">
-          <img src="../assets/Images/Hufflepuff.png" width="100%" @click="Houses('hufflepuff')"/>
-        </b-col>
-      </b-row>
-      <b-row>
-          <b-col cols="3">
-              <House :house="houseName" v-if="houseName !== null"></House>
-          </b-col>
-      </b-row>
+    <b-row class="mt-4">
+      <b-col cols="3">
+        <img
+          src="../assets/Images/Gryffindor.png"
+          width="100%"
+          @click="Houses('gryffindor')"
+        />
+      </b-col>
+      <b-col cols="3">
+        <img
+          src="../assets/Images/Slytherin.png"
+          width="100%"
+          @click="Houses('slytherin')"
+        />
+      </b-col>
+      <b-col cols="3">
+        <img
+          src="../assets/Images/Ravenclaw.png"
+          width="100%"
+          @click="Houses('ravenclaw')"
+        />
+      </b-col>
+      <b-col cols="3">
+        <img
+          src="../assets/Images/Hufflepuff.png"
+          width="100%"
+          @click="Houses('hufflepuff')"
+        />
+      </b-col>
+    </b-row>
+    <House :house="houseName" v-if="houseName !== null"></House>
   </b-container>
 </template>
 
 <script>
-import House from '@/components/House';
+import axios from "axios";
+import House from "@/components/House";
 export default {
-    components: {
-        House
+  components: {
+    House,
+  },
+  data() {
+    return {
+      houseName: null
+    };
+  },
+  methods: {
+    async Houses(house) {
+      this.houseName = house;
+      let data = await axios.get(
+        `http://hp-api.herokuapp.com/api/characters/house/${this.houseName}`
+      );
+      this.$store.state.houseStudents = data.data;
     },
-    data() {
-        return {
-            houseName: null
-        }
-    },
-    methods: {
-        Houses(house) {
-            this.houseName = house;
-        }
-    }
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
